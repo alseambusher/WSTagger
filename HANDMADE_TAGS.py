1a5c8317bb53627905d9fc91fb9561321ddb351c
Suresh Alse <alseambusher@gmail.com>
2013-04-25 11:53:51 +0530

    done

--- HANDMADE_TAGS.py
+++ HANDMADE_TAGS.py
@@ -1,21 +1,21 @@
 from config import SERVICES_FOLDER
-HANDMADE_TAGS={SERVICES_FOLDER+"_3WheeledAudiCarprice_service.wsdl":[],\
+HANDMADE_TAGS={SERVICES_FOLDER+"_3WheeledAudiCarprice_service.wsdl":['JJ','3wheel audi car','price'],\
                SERVICES_FOLDER+"Academic-degreeFunding.wsdl":[],\
-               SERVICES_FOLDER+"CarPriceauto.wsdl":[],\
-               SERVICES_FOLDER+"car_pricecolor_service.wsdl":[],\
-               SERVICES_FOLDER+"CarPricequality.wsdl":[],\
+               SERVICES_FOLDER+"CarPriceauto.wsdl":['carprice','giftauto'],\
+               SERVICES_FOLDER+"car_pricecolor_service.wsdl":['T-c\Car','color price','color','price'],\
+               SERVICES_FOLDER+"CarPricequality.wsdl":['car','price','quality'],\
                SERVICES_FOLDER+"car_pricereport_service.wsdl":[],\
-               SERVICES_FOLDER+"CountryDeacon.wsdl":[],\
-               SERVICES_FOLDER+"CountryDrought.wsdl":[],\
-               SERVICES_FOLDER+"CountryHotel.wsdl":[],\
-               SERVICES_FOLDER+"CountryLightning.wsdl":[],\
+               SERVICES_FOLDER+"CountryDeacon.wsdl":['deaconposition','deacon','country'],\
+               SERVICES_FOLDER+"CountryDrought.wsdl":['drought','drought-information','country'],\
+               SERVICES_FOLDER+"CountryHotel.wsdl":['hotel list','country'],\
+               SERVICES_FOLDER+"CountryLightning.wsdl":['lightning-info','lightning','country'],\
                SERVICES_FOLDER+"CountryMap.wsdl":[],\
-               SERVICES_FOLDER+"FoodPrice1.wsdl":[],\
-               SERVICES_FOLDER+"FoodPricephysical-quantity.wsdl":[],\
-               SERVICES_FOLDER+"FoodPricequantity.wsdl":[],\
-               SERVICES_FOLDER+"FoodPrice.wsdl":[],\
+               SERVICES_FOLDER+"FoodPrice1.wsdl":['food item price','food item','food','price'],\
+               SERVICES_FOLDER+"FoodPricephysical-quantity.wsdl":['food item price','approximate price','available quantity','quantity','price'],\
+               SERVICES_FOLDER+"FoodPricequantity.wsdl":['food item price','available quantity','price'],\
+               SERVICES_FOLDER+"FoodPrice.wsdl":['food item','price'],\
                SERVICES_FOLDER+"FoodRecommendedprice.wsdl":[],\
-               SERVICES_FOLDER+"government_funding_BallMissileservice.wsdl":[],\
-               SERVICES_FOLDER+"government_funding_ForPhDservice.wsdl":[],\
-               SERVICES_FOLDER+"government_funding_Missileservice.wsdl":[],\
+               SERVICES_FOLDER+"government_funding_BallMissileservice.wsdl":['funding','ballistic','missile','government'],\
+               SERVICES_FOLDER+"government_funding_ForPhDservice.wsdl":['funding','PhD','government'],\
+               SERVICES_FOLDER+"government_funding_Missileservice.wsdl":['funding','missile','government'],\
                SERVICES_FOLDER+"government_lending_service.wsdl":[]}
--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import clustering
 import extraction
 import config
 import os
+import time
 from wsdl import WSDL
-
+import json
+os.system("figlet DISTANCE MATRIX")
 distance_matrix=clustering.get_distance_matrix()
-print distance_matrix
+for distance in distance_matrix.iterkeys():
+    print distance
+    print distance_matrix[distance]
+    print ""
+old_distance_matrix=json.dumps(distance_matrix)
 clusters=clustering.get_all_clusters(distance_matrix)
-print "\nCLUSTERS\n"
-print clusters
+os.system("figlet CLUSTERS")
+time.sleep(1)
+for cluster in clusters:
+    print cluster
 
 #parse WSDL
 wsdl_object_array=[]
@@ -19,13 +27,28 @@ for service in os.listdir(config.SERVICES_FOLDER):
 token_weight={}
 for wsdl in wsdl_object_array:
     token_weight[wsdl.file_name]=extraction.total_token_weight(wsdl,clusters)
+os.system("figlet TAGS")
+time.sleep(1)
+for tok_weight in token_weight.iterkeys():
+    print tok_weight
+    print token_weight[tok_weight]
+    print ""
+
 
 #print "\nTOKEN WEIGHT\n"
 #print token_weight
 #enrich tags for all wsdl
 enriched_tags={}
 for wsdl in wsdl_object_array:
-    enriched_tags[wsdl.file_name]=extraction.tag_enriching(wsdl,clusters,distance_matrix)
+    #enriched_tags[wsdl.file_name]=extraction.tag_enriching(wsdl,clusters,old_distance_matrix)
+    enriched_tags[wsdl.file_name]=extraction.tag_enriching(wsdl,clusters,eval(old_distance_matrix))
+os.system("figlet ENRICHED TAGS")
+time.sleep(1)
+for enriched in enriched_tags.iterkeys():
+    print enriched
+    print enriched_tags[enriched]
+    print ""
+
 
 #print "\nENRICHED TAGS\n"
 #print enriched_tags
