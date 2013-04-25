c74141c745f5818c3e4b7b3eba34cd108c03558d
Suresh Alse <alseambusher@gmail.com>
2013-04-25 05:30:28 +0530

    hand made tags should be added along with documentation

--- HANDMADE_TAGS.py
+++ HANDMADE_TAGS.py
@@ -1,21 +1,21 @@
 from config import SERVICES_FOLDER
-HANDMADE_TAGS={SERVICES_FOLDER+"1personbicyclecar_price_TheBestservice.wsdl":[],\
-               SERVICES_FOLDER+"academic-degreegovernment_funding_service.wsdl":[],\
-               SERVICES_FOLDER+"CareorganizationDiagnosticprocesstimemeasure.wsdl":[],\
-               SERVICES_FOLDER+"CareorganizationDiagnosticprocesstimeinterval.wsdl":[],\
-               SERVICES_FOLDER+"amount-of-money3wheeledcar_price_service.wsdl":[],\
-               SERVICES_FOLDER+"CareorganizationDiagnosticprocess.wsdl":[],\
-               SERVICES_FOLDER+"Amount-of-moneycarPrice.wsdl":[],\
-               SERVICES_FOLDER+"amount-of-money3wheeledcar_recommendedprice_service.wsdl":[],\
-               SERVICES_FOLDER+"Academic-degreegovernmentorganizationLending.wsdl":[],\
-               SERVICES_FOLDER+"Academic-degreegovernmentLending.wsdl":[],\
-               SERVICES_FOLDER+"CareorganizationDiagnosticprocesstimeduration.wsdl":[],\
-               SERVICES_FOLDER+"amount-of-money4wheeledcar_price_service.wsdl":[],\
-               SERVICES_FOLDER+"CareorganizationDiagnosticprocesslengthmeasure.wsdl":[],\
-               SERVICES_FOLDER+"academic-degreegovernment_scholarship_service.wsdl":[],\
-               SERVICES_FOLDER+"academic-degreegovernmentorganization_funding_service.wsdl":[],\
-               SERVICES_FOLDER+"1personbicycle4wheeledcar_price_service.wsdl":[],\
-               SERVICES_FOLDER+"_3WheeledAudiCarprice_service.wsdl":[],\
-               SERVICES_FOLDER+"amount-of-money4wheeledcar_recommendedprice_service.wsdl":[],\
+HANDMADE_TAGS={SERVICES_FOLDER+"_3WheeledAudiCarprice_service.wsdl":[],\
                SERVICES_FOLDER+"Academic-degreeFunding.wsdl":[],\
-               SERVICES_FOLDER+"1personbicyclecar_price_Kohlservice.wsdl":[]}
+               SERVICES_FOLDER+"CarPriceauto.wsdl":[],\
+               SERVICES_FOLDER+"car_pricecolor_service.wsdl":[],\
+               SERVICES_FOLDER+"CarPricequality.wsdl":[],\
+               SERVICES_FOLDER+"car_pricereport_service.wsdl":[],\
+               SERVICES_FOLDER+"CountryDeacon.wsdl":[],\
+               SERVICES_FOLDER+"CountryDrought.wsdl":[],\
+               SERVICES_FOLDER+"CountryHotel.wsdl":[],\
+               SERVICES_FOLDER+"CountryLightning.wsdl":[],\
+               SERVICES_FOLDER+"CountryMap.wsdl":[],\
+               SERVICES_FOLDER+"FoodPrice1.wsdl":[],\
+               SERVICES_FOLDER+"FoodPricephysical-quantity.wsdl":[],\
+               SERVICES_FOLDER+"FoodPricequantity.wsdl":[],\
+               SERVICES_FOLDER+"FoodPrice.wsdl":[],\
+               SERVICES_FOLDER+"FoodRecommendedprice.wsdl":[],\
+               SERVICES_FOLDER+"government_funding_BallMissileservice.wsdl":[],\
+               SERVICES_FOLDER+"government_funding_ForPhDservice.wsdl":[],\
+               SERVICES_FOLDER+"government_funding_Missileservice.wsdl":[],\
+               SERVICES_FOLDER+"government_lending_service.wsdl":[]}
